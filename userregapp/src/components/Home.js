123c73a145189e55fc4af802cbe15a3135c5bb2e
Rajesh4027 <rajeshdevaraj44@gmail.com>
2024-03-31 09:48:27 +0530

    nav bar adding

--- userregapp/src/components/Home.js
+++ userregapp/src/components/Home.js
@@ -1,10 +1,21 @@
 import React from "react"
+import { IoReorderThreeOutline, IoSearch } from "react-icons/io5";
 function Home() {
     return(
         <div className="homepage">
-
-            <h1>Hello  and welcome to the home </h1>
+             <div>
+                <p></p>
+                 <a href="">Home</a>
+                 <a href="">Course</a>
+                 <a href="">Batches</a>
+                 <a href="">Contacts</a>
+                 <a href="">Placements</a>
+                 <a href="">Enquery</a>
+                 <a href="" id="hel"><IoReorderThreeOutline /></a>
+                 <a href="" id="hell"><IoSearch /></a>
+             </div>
             {/* {location.state.id} */}
+
         </div>
     )
 }
